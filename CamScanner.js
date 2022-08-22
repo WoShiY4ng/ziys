c03e036e2691ba911c49f1d1d4f5d4db12dab94a
WoShiY4ng <53864724+WoShiY4ng@users.noreply.github.com>
2022-08-22 09:10:36 +0800

    Update CamScanner.js
    扫描全能王 破解

--- CamScanner.js
+++ CamScanner.js
@@ -5,7 +5,7 @@ CamScanner 解锁部分高级特权
 Surge4 or Loon:
 
 [Script]
-http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=CamScanner.js
+http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=扫描全能王.js
 
 [MITM]
 hostname = ap*.intsig.net
